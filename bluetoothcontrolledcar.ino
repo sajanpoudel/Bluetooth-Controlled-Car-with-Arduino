579a51a5083d47e86fa3ea8d5ca8ab3f6efe2664
Sajan Poudel <35656849+sajanpoudel@users.noreply.github.com>
2021-09-24 21:46:45 +0545

    Create bluetoothcontrolledcar.ino

---
+++ bluetoothcontrolledcar.ino
@@ -0,0 +1,97 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(10, 11); //TX, RX respetively
+String readdata;
+int motorLpin1=2;
+int motorLpin2=3;
+int motorRpin1=4;
+int motorRpin2=5;
+int motorLpwm=10;
+int motorRpwm=11;
+
+int motorSpeed=125;
+int turn=50;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.flush();
+  pinMode(motorLpin1,OUTPUT);
+  pinMode(motorLpin2,OUTPUT);
+  pinMode(motorRpin1,OUTPUT);
+  pinMode(motorRpin2,OUTPUT);
+  pinMode(motorLpwm,OUTPUT);
+  pinMode(motorRpwm,OUTPUT);
+}
+
+void loop() {
+  String input="";
+  while(Serial.available()){
+    input+=(char)Serial.read();
+    delay(5);
+  }
+  
+  if(input=="forward"){
+    fwd();
+  }
+  else if(input=="stop"){
+    stp();
+  }
+  else if(input=="backward"){
+    rev();
+  }
+  else if(input.indexOf("left")>-1){
+    lft();
+  }
+  else if(input.indexOf("right")>-1){
+    rght();
+  }
+  else if(input!=""){
+    motorSpeed=input.toInt();
+  }
+}
+
+void fwd(){
+  analogWrite(motorLpwm,motorSpeed);
+  analogWrite(motorRpwm,motorSpeed);
+  digitalWrite(motorLpin1,1);
+  digitalWrite(motorLpin2,0);
+  digitalWrite(motorRpin1,1);
+  digitalWrite(motorRpin2,0);
+}
+
+void rev(){
+  analogWrite(motorLpwm,motorSpeed);
+  analogWrite(motorRpwm,motorSpeed);
+  digitalWrite(motorLpin1,0);
+  digitalWrite(motorLpin2,1);
+  digitalWrite(motorRpin1,0);
+  digitalWrite(motorRpin2,1);
+}
+
+void lft(){
+  analogWrite(motorLpwm,motorSpeed-turn);
+  analogWrite(motorRpwm,motorSpeed+turn);
+  digitalWrite(motorLpin1,0);
+  digitalWrite(motorLpin2,1);
+  digitalWrite(motorRpin1,1);
+  digitalWrite(motorRpin2,0);
+}
+
+void rght(){
+  analogWrite(motorLpwm,motorSpeed+turn);
+  analogWrite(motorRpwm,motorSpeed-turn);
+  digitalWrite(motorLpin1,1);
+  digitalWrite(motorLpin2,0);
+  digitalWrite(motorRpin1,0);
+  digitalWrite(motorRpin2,1);
+}
+
+void stp(){
+  analogWrite(motorLpwm,0);
+  analogWrite(motorRpwm,0);
+  digitalWrite(motorLpin1,1);
+  digitalWrite(motorLpin2,1);
+  digitalWrite(motorRpin1,1);
+  digitalWrite(motorRpin2,1);
+}
+
